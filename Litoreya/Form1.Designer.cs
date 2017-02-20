59ede6f98efc8fa7918bed5a8a9210a54281664f
Алексей Геть < >
2017-02-20 23:01:00 +0800

    Last commit

--- Litoreya/Form1.Designer.cs
+++ Litoreya/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Litoreya
 {
-    partial class Form1
+    partial class Litoreya
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,12 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.encodeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(15, 40);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(300, 80);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(15, 15);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(34, 13);
+            this.inputLabel.TabIndex = 1;
+            this.inputLabel.Text = "Input:";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(15, 139);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(42, 13);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "Output:";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(15, 164);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(300, 80);
+            this.outputTextBox.TabIndex = 2;
+            // 
+            // encodeButton
+            // 
+            this.encodeButton.Location = new System.Drawing.Point(15, 267);
+            this.encodeButton.Name = "encodeButton";
+            this.encodeButton.Size = new System.Drawing.Size(75, 23);
+            this.encodeButton.TabIndex = 4;
+            this.encodeButton.Text = "Encode";
+            this.encodeButton.UseVisualStyleBackColor = true;
+            this.encodeButton.Click += new System.EventHandler(this.encodeButton_Click);
+            // 
+            // Litoreya
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(331, 303);
+            this.Controls.Add(this.encodeButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Litoreya";
+            this.Text = "Litoreya";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button encodeButton;
     }
 }
 
--- Litoreya/Form1.cs
+++ Litoreya/Form1.cs
@@ -10,11 +10,48 @@ using System.Windows.Forms;
 
 namespace Litoreya
 {
-    public partial class Form1 : Form
+    public partial class Litoreya : Form
     {
-        public Form1()
+        public Litoreya()
         {
             InitializeComponent();
+        }
+
+        private string consonantsPartOne = "бвгджзклмн";
+        private string consonantsPartTwo = "щшчцхфтсрп";
+
+        private string encode(string encrypted)
+        {          
+            for (int i = 0; i < encrypted.Length; i++)
+            {
+                int position = consonantsPartOne.IndexOf(encrypted[i]);
+
+                if (position != -1)
+                {
+                    encrypted = encrypted
+                        .Remove(i, 1)
+                        .Insert(i, consonantsPartTwo[position].ToString());
+
+                    continue;
+                }
+
+                position = consonantsPartTwo.IndexOf(encrypted[i]);
+
+                if (position != -1)
+                {
+                    encrypted = encrypted
+                        .Remove(i, 1)
+                        .Insert(i, consonantsPartOne[position].ToString());
+                }
+            }
+
+            return encrypted;
+        }
+
+        private void encodeButton_Click(object sender, EventArgs e)
+        {
+            string input = inputTextBox.Text.ToLower();
+            outputTextBox.Text = encode(input);
         }
     }
 }
